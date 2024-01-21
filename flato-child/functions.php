ca052955fd30d0677d61afefa238261254de7fa0
Rob <robgroves0.gmailcom>
2024-01-21 15:09:04 -0500

    fix: adding flato-child/option-tree/includes/ot-functions-admin.php to overwrite the same file in the parent theme didnt work, so just recreted the offending function ot_register_theme_options_page() in the child theme functions.php file. child theme functions are loaded first, so they override if the parent theme funstion is wrapped in a check to see if it is already loaded

--- flato-child/functions.php
+++ flato-child/functions.php
@@ -9,6 +9,63 @@ function my_theme_enqueue_styles() {
 	);
 	*/
 
+	function ot_register_theme_options_page() {
+  
+		/* get the settings array */
+		$get_settings = get_option( 'option_tree_settings' );
+		
+		/* sections array */
+		$sections = isset( $get_settings['sections'] ) ? $get_settings['sections'] : array();
+		
+		/* settings array */
+		$settings = isset( $get_settings['settings'] ) ? $get_settings['settings'] : array();
+		
+		/* contexual_help array */
+		$contextual_help = isset( $get_settings['contextual_help'] ) ? $get_settings['contextual_help'] : array();
+		
+		/* build the Theme Options */
+		if ( function_exists( 'ot_register_settings' ) && OT_USE_THEME_OPTIONS ) {
+		  
+		  ot_register_settings( array(
+			  array(
+				'id'                  => 'option_tree',
+				'pages'               => array( 
+				  array(
+					'id'              => 'ot_theme_options',
+					'parent_slug'     => apply_filters( 'ot_theme_options_parent_slug', 'themes.php' ),
+					'page_title'      => apply_filters( 'ot_theme_options_page_title', __( 'Theme Options', 'option-tree' ) ),
+					'menu_title'      => apply_filters( 'ot_theme_options_menu_title', __( 'Theme Options', 'option-tree' ) ),
+					'capability'      => $caps = apply_filters( 'ot_theme_options_capability', 'edit_theme_options' ),
+					'menu_slug'       => apply_filters( 'ot_theme_options_menu_slug', 'ot-theme-options' ),
+					'icon_url'        => apply_filters( 'ot_theme_options_icon_url', null ),
+					'position'        => apply_filters( 'ot_theme_options_position', null ),
+					'updated_message' => apply_filters( 'ot_theme_options_updated_message', __( 'Theme Options updated.', 'option-tree' ) ),
+					'reset_message'   => apply_filters( 'ot_theme_options_reset_message', __( 'Theme Options reset.', 'option-tree' ) ),
+					'button_text'     => apply_filters( 'ot_theme_options_button_text', __( 'Save Changes', 'option-tree' ) ),
+					'screen_icon'     => 'themes',
+					'contextual_help' => $contextual_help,
+					'sections'        => $sections,
+					'settings'        => $settings
+				  )
+				)
+			  )
+			) 
+		  );
+		  
+		  // Filters the options.php to add the minimum user capabilities.
+		  // replace the below create_function with an anonymous function because create_function() is deprecated and causes 
+		  // fatal errors in PHP 8.0
+		  //add_filter( 'option_page_capability_option_tree', create_function( '$caps', "return '$caps';" ), 999 );
+		  add_filter( 'option_page_capability_option_tree', 
+		  function( $caps ) {
+			return $caps;
+		  }
+		  , 999 );
+		
+		}
+	  
+	}
+
 function flato_child_enqueue_styles() {
 	$theme        = wp_get_theme();
 	$parenthandle = 'themememe-base';
